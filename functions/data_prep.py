d824d16de908ab93a97e27c5a6db92e7ddd05691
Irena Zimovska <irenazimovska@gmail.com>
2024-05-23 00:05:31 +0200

    developing predicting model

--- functions/data_prep.py
+++ functions/data_prep.py
@@ -10,24 +10,34 @@ class DataPreparation:
     def check_missing_data(self): 
         """Verify if missing data is persistent - if yes, returns column name."""
         missing_cols = self.df.columns[self.df.isnull().any()]
-        return missing_cols
+
+        if missing_cols.empty: 
+            return None
+        else: 
+            return missing_cols
 
     def impute_missing_data(self, missing_cols, impute_option:str): 
         """Complementary function - takes column name from the previous function and imputes."""
 
-        if impute_option == 'median': 
-            self.df[[missing_cols]] = self.df[[missing_cols]].fillna(self.df[[missing_cols]].median())
+        if missing_cols is not None: 
 
-        elif impute_option == 'zero': 
-            self.df[[missing_cols]] = self.df[[missing_cols]].fillna(0)
+            if impute_option == 'median': 
+                self.df[missing_cols] = self.df[missing_cols].fillna(self.df[missing_cols].median())
 
-    def correct_column_value(self, column_name:str): 
+            elif impute_option == 'zero': 
+                self.df[missing_cols] = self.df[missing_cols].fillna(0)
+
+        return self.df
+
+    def correct_column_value(self): 
         """Any other corrections on data."""
         self.df['department'] = self.df['department'].str.strip()
-        self.df = pd.get_dummies(self.df, columns=['department'])
+        self.df = pd.get_dummies(self.df, columns=['department'], drop_first=True)
+        # self.df = pd.get_dummies(self.df, columns=['department'])
         self.df['day'] = pd.to_datetime(self.df['date']).apply(lambda x: x.day)
         self.df['month'] = pd.to_datetime(self.df['date']).apply(lambda x: x.month)
         self.df['num_week'] = pd.to_datetime(self.df['date']).apply(lambda x: x.weekofyear)
+        self.df.drop(['quarter'], axis=1, inplace=True)
 
         return self.df
 
--- functions/prediction.py
+++ functions/prediction.py
@@ -1,9 +1,94 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+import logging
+
 class ProductivityPredictModel: 
     """
     Class that performs the prediction of productivity with XgBoost model.  
     """
-    def __init__(self, df, dep_var): 
-        self.df = df 
-        self.dep_var = dep_var
+    def __init__(self, max_depth=None, n_estimators=100, learning_rate=0.1):
+        self.model = XGBRegressor(max_depth=max_depth, n_estimators=n_estimators, learning_rate=learning_rate)
+        self.lag_features = None
+        logging.info("Initialized TimeSeriesRegressionTree with max_depth={}, n_estimators={}, learning_rate={}",
+                    max_depth, n_estimators, learning_rate)
+
+    def create_lag_features(self, data, features, lags):
+        """
+        Creates lag features for specific features in the time series data.
+        
+        Parameters:
+        data (pd.DataFrame): The input time series data.
+        features (list): The list of feature names to create lag features for.
+        lags (int): The number of lag features to create.
+
+        Returns:
+        pd.DataFrame: DataFrame with lag features.
+        """
+        self.lag_features = lags
+        df_lagged = data.copy()
+        for feature in features:
+            for lag in range(1, lags + 1):
+                df_lagged[f'{feature}_lag_{lag}'] = data[feature].shift(lag)
+        df_lagged.dropna(inplace=True)
+        logging.info("Created lag features for features={} with lags={}", features, lags)
+
+        return df_lagged
+
+    
+    def fit(self, data, features, lags, target, split_date, date_column):
+        """
+        Fits the regression tree model on the time series data with lag features.
+        
+        Parameters:
+        data (pd.DataFrame): The input time series data.
+        features (list): The list of feature names to create lag features for.
+        lags (int): The number of lag features to create.
+        target (str): The name of the target column.
+        split_date (str): The date to split the data into train and test sets.
+        """
+        df_lagged = self.create_lag_features(data, features, lags)
+        train = df_lagged[df_lagged[date_column] < split_date]
+        test = df_lagged[df_lagged[date_column] >= split_date]
+
+        X_train = train.drop(columns=[target, date_column])
+        y_train = train[target]
+        X_test = test.drop(columns=[target, date_column])
+        y_test = test[target]
+
+        self.model.fit(X_train, y_train)
+        train_score = self.model.score(X_train, y_train)
+        test_score = self.model.score(X_test, y_test)
+        logging.info("Model trained with R^2 score on training data: {:.4f}", train_score)
+        logging.info("Model trained with R^2 score on test data: {:.4f}", test_score)
+
+        self.X_test = X_test
+        self.y_test = y_test
+
+
+    def predict(self):
+        """
+        Makes predictions on the test data.
+        
+        Returns:
+        np.ndarray: Array of predictions.
+        """
+        if self.lag_features is None or self.X_test is None:
+            logging.error("The model has not been fitted yet or test data is not available.")
+            raise ValueError("The model has not been fitted yet or test data is not available.")
+        
+        predictions = self.model.predict(self.X_test)
+        logging.info("Made predictions on the test data")
+
+        df_predictions = self.X_test.copy()
+        df_predictions['actual'] = self.y_test
+        df_predictions['predicted'] = predictions
+
+        logging.info("Made predictions on the test data")
+        return df_predictions
+
+    def evaluate(self): 
+        pass
 
     
